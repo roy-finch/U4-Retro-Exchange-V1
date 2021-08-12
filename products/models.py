c3f326ba5e625c703c09c859ee9f8b74e78ef19b
roy-finch <royfinch99@hotmail.com>
2021-08-12 22:19:13 +0000

    I have added products, the models and the json file

--- products/models.py
+++ products/models.py
@@ -3,13 +3,23 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    display_name = models.CharField(max_length=254, null=True, blank=True)
+    display_name = models.CharField(max_length=254, blank=True)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
     def get_friendly_name(self):
         return self.display_name
 
 
 class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
