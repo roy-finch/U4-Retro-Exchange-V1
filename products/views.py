09ef4356b0fc9d0441f04b56961a223f6fda70b8
roy-finch <royfinch99@hotmail.com>
2021-08-19 05:13:24 +0000

    got the images to load on the website and also I am testing the select/option filter instead of input

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product
 
 # Create your views here.
@@ -12,26 +13,62 @@ def all_products(request):
 
     products = Product.objects.all()
 
+    search = None
+    search_num = None
+    ids = [0, 0]
+    query = None
+    sortkey = None
+
     if request.GET:
-        query = [request.GET["c"], request.GET["q"], request.GET["f"]]
-        if not query:
-            return redirect("home")
-        ids = [consoles.index(query[0]), categories.index(query[1])]
+        if request.GET["search"] is None or request.GET["search"] == "":
+            query = [request.GET["c"], request.GET["q"], request.GET["f"]]
+            if query[2] != "":
+                if query[2] == "Alphabetical":
+                    sortkey = f'{"lower_name"}'
+                    products = products.annotate(lower_name=Lower("name"))
+                if query[2] == "Ascending Price":
+                    sortkey = f'{"price"}'
+                if query[2] == "Decending Price":
+                    sortkey = f'-{"price"}'
+                if query[2] == "Rating":
+                    sortkey = f'{"rating"}'
+                products = products.order_by(sortkey)
+            if not query:
+                return redirect("home")
+            
+            if query[0] in consoles:
+                ids[0] = consoles.index(query[0])
+            else:
+                ids[0] = 0
+                query[0] = "No Specific Console"
+            if query[1] in categories:
+                ids[1] = categories.index(query[1])
+            else:
+                ids[1] = 0
+                query[1] = "All"
 
-        if query[0] == "No Specific Console":
-            queries = Q(category=ids[1])
-        elif query[1] == "All":
-            queries = Q(console=ids[0])
+            if query[0] == "No Specific Console" and query[1] != "All":
+                queries = Q(category=ids[1])
+            elif query[1] == "All" and query[0] != "No Specific Console":
+                queries = Q(console=ids[0])
+            else:
+                queries = Q(console=ids[0]) & Q(category=ids[1])
+
+            if (query[0] != "No Specific Console"):
+                products = products.filter(queries)
+            else:
+                products = products
         else:
-            queries = Q(console=ids[0]) & Q(category=ids[1])
-
-        if query[0] != "No Specific Console" or query[1] != "All":
+            search = request.GET["search"]
+            queries = Q(
+                name__icontains=search) | Q(description__icontains=search)
             products = products.filter(queries)
-        else:
-            products = products
+            search_num = len(products)
 
     context = {
         "products": products,
+        "search_q": search,
+        "return_num": search_num,
     }
 
     return render(request, "products/products.html", context)
