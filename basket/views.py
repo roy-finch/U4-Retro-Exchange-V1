b6d2a7d0f111828cbbda07e74bb3b62cfef3c47f
roy-finch <royfinch99@hotmail.com>
2021-08-20 00:16:10 +0000

    created basket page and will add content

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_basket(request):
+    """ This returns the index page """
+
+    return render(request, "basket/basket.html")
--- retro_exchange/settings.py
+++ retro_exchange/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     # added apps
     'home',
     'products',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # allauth rq
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -156,6 +158,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
