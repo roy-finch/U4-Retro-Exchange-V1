b90b1ec13352a90eab7136b89923104e577911c4
roy-finch <royfinch99@hotmail.com>
2021-09-05 13:39:17 +0000

    added a listener to the stripe js

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,11 +5,19 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ("full_name", "email", "phone_number", "street_add_line1", "street_add_line2", "town_r_city", "postcode", "country", "county",)
+        fields = ("full_name", "email", "phone_number", "street_add_line1",
+                  "street_add_line2", "town_r_city", "postcode",
+                  "country", "county",)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        placeholders = {"full_name": "Full Name", "email": "Email Address", "phone_number": "Phone Number", "country": "Country", "county": "County", "street_add_line1": "First Line of your Address", "street_add_line2": "Second Line of your Address", "town_r_city": "Town / City", "postcode": "Postal Code"}
+        placeholders = {"full_name": "Full Name", "email": "Email Address",
+                        "phone_number": "Phone Number", "country": "Country",
+                        "county": "County",
+                        "street_add_line1": "First Line of your Address",
+                        "street_add_line2": "Second Line of your Address",
+                        "town_r_city": "Town / City",
+                        "postcode": "Postal Code"}
 
         self.fields["full_name"].widget.attrs["autofocus"] = True
         for field in self.fields:
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,5 +1,7 @@
 /*
     This is to create and enable the stripe smart checkout.
+    Used https://stripe.com.docs/payments/accept-a-payment
+    to create functionality of the payment methods.
 */
 
 var stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
@@ -12,9 +14,20 @@ var style = {
         fontFamily: '"Segoe UI", sans-serif',
     },
     invalid: {
-        color: 'red',
+        color: "red",
     }
 };
 
-var card = elements.create("card", {style: style});
-card.mount("#card-div");
+var card = elements.create("card");
+card.mount("#card-element", {style: style});
+
+card.addEventListener("change", function(event) {
+    var errorC = document.getElementById("card-errors");
+    if (event.error) {
+        var html =
+            <span class="error">${ event.error.message }</span>
+        $(errorC).html(html)
+    } else {
+        errorC.textContent = '';
+    }
+});
