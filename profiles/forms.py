acf25eb1e88ad39b2d7be5e5560de0225e9a5fbf
roy-finch <royfinch99@hotmail.com>
2021-09-06 19:50:53 +0000

    made the profile page render a form and allowed for a correct message display

---
+++ profiles/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import UserProfile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            "default_phone_number": "Phone Number",
+            "default_county": "County, State or Locality",
+            "default_street_add_line1": "First Line of your Address",
+            "default_street_add_line2": "Second Line of your Address",
+            "default_town_r_city": "Town / City",
+            "default_postcode": "Postal Code"}
+
+        self.fields["default_phone_number"].widget.attrs["autofocus"] = True
+        for field in self.fields:
+            if field != "default_country":
+                if self.fields[field].required:
+                    placeholder = f"{placeholders[field]}"
+                else:
+                    placeholder = placeholders[field]
+            self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
-
+from .forms import ProfileForm
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile update successfully')
+
+    form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profile.html'
     context = {
-        "profile": profile
+        "profile": profile,
+        "form": form,
+        "orders": orders,
+        "check_profile": True
     }
 
     return render(request, template, context)
