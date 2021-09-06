d0a912e3e8a6ccbff00af682c88f200d71b30d4b
roy-finch <royfinch99@hotmail.com>
2021-09-06 14:10:06 +0000

    changed the data field to countries for the country field on the payment page

--- checkout/forms.py
+++ checkout/forms.py
@@ -12,8 +12,8 @@ class OrderForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         placeholders = {"full_name": "Full Name", "email": "Email Address",
-                        "phone_number": "Phone Number", "country": "Country",
-                        "county": "County",
+                        "phone_number": "Phone Number",
+                        "county": "County, State or Locality",
                         "street_add_line1": "First Line of your Address",
                         "street_add_line2": "Second Line of your Address",
                         "town_r_city": "Town / City",
@@ -21,9 +21,10 @@ class OrderForm(forms.ModelForm):
 
         self.fields["full_name"].widget.attrs["autofocus"] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f"{placeholders[field]}"
-            else:
-                placeholder = placeholders[field]
+            if field != "country":
+                if self.fields[field].required:
+                    placeholder = f"{placeholders[field]}"
+                else:
+                    placeholder = placeholders[field]
             self.fields[field].widget.attrs["placeholder"] = placeholder
             self.fields[field].label = False
