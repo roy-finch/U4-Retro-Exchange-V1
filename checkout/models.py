89799cd505e33bbaa08d160b1d920c4eb07bff79
roy-finch <royfinch99@hotmail.com>
2021-09-06 19:10:25 +0000

    added a profile app and page.

--- checkout/models.py
+++ checkout/models.py
@@ -6,10 +6,14 @@ from products.models import Product
 from django.conf import settings
 
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, null=True, blank=True,
+                                     on_delete=models.CASCADE,
+                                     related_name="orders")
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
---
+++ profiles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+
+
+def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    template = 'profile.html'
+    context = {
+        "profile": profile
+    }
+
+    return render(request, template, context)
