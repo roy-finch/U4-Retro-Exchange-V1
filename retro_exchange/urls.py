b981ad6ae6d500ebce582448badf60c633e1acf6
roy-finch <royfinch99@hotmail.com>
2021-09-13 15:11:36 +0000

    alter settings

--- retro_exchange/urls.py
+++ retro_exchange/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('/', include('home.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
