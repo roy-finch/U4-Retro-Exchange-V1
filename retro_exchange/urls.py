ef602f1b0e47d71c64ba818b79985a02239eb5b1
roy-finch <royfinch99@hotmail.com>
2021-09-13 19:23:14 +0000

    test

--- retro_exchange/urls.py
+++ retro_exchange/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
