5d7d43d8e2e27fafa32c1fa0cd0d97e7aa48f271
roy-finch <royfinch99@hotmail.com>
2021-09-13 12:32:24 +0000

    corrected spelling mistake

--- retro_exchange/settings.py
+++ retro_exchange/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = 'retro_exchange.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATATBASES = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
